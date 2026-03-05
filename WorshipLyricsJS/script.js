ed1243d98d1775feefdf253a092e2fa0523d0669
Nico Castro <castroconi101@gmail.com>
2026-03-05 12:53:09 -0500

    FINISHED THE JS

--- WorshipLyricsJS/script.js
+++ WorshipLyricsJS/script.js
@@ -11,18 +11,36 @@ fetch("songs.json")
     return response.json();
   })
   .then(data => {
-    
     for(const song of data){
         songCollection.push(song)
     }
     console.log(songCollection);
+    Show(songCollection);
+  })
+  .catch(error => {
+    console.error(error);
+});
 
-    
-  songCollection.forEach(song => {
+const inputSongs = document.getElementById("search-input");
+inputSongs.addEventListener("input", (e) => {
+  const inputValue = e.target.value.toLowerCase();
+ console.log(inputValue);
+ FilterSongs(inputValue)
+})
+
+const sortSongs = document.getElementById("dropdownMenu");
+sortSongs.addEventListener("change", (e) => {
+  const sortValue = e.target.value;
+  console.log(sortValue);
+  SortSongs(sortValue);
+})
+
+function Show(songs){
+  songShelf.innerHTML = "";
+  songs.forEach(song => {
     const column = document.createElement("div");
-
+    column.classList.add("col-12", "col-sm-6", "col-lg-4");
     column.innerHTML = `
-     <div class="col-12 col-sm-6 col-lg-4">
         <div class="card p-3 text-black">
           <p class="title mb-0 fw-bold">${song.title ? song.title : "Title of the Song"}</p>
                     <hr class="m-0 ">
@@ -32,13 +50,41 @@ fetch("songs.json")
        </a>
            <a href=${song.presentationLink ? song.presentationLink : "#"} class=" text-center m-2 fw-bold">Song Presentation Link</a>
         </div>
-      </div>
     `
-
     songShelf.appendChild(column);
   });
+}
 
+function FilterSongs(request){
+  
+  const filteredSongs = songCollection.filter(song => {
+    return song.title.toLowerCase().includes(request);
   })
-  .catch(error => {
-    console.error(error);
-});
+
+  if(!filteredSongs.length){
+    songShelf.innerHTML = `<p class="sorry">We're sorry.. the lyrics you are trying to find is not available.</p>`
+  } else {
+  Show(filteredSongs);
+  }
+}
+
+function SortSongs(request){
+  let sortedSongs = [];
+
+  switch(request){
+    case "Default":
+        Show(songCollection);
+      break;
+    case "Oldest" :
+        Show(songCollection.toReversed());
+      break;
+    case "A-Z" :
+      sortedSongs = songCollection.toSorted((a, b) =>
+        a.title.localeCompare(b.title)
+      );
+         Show(sortedSongs);
+      break;
+  }
+
+
+}
